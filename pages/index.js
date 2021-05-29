f978b6c4ab1629b56847f20db90e693084a563aa
Barbora Drachovská <bdrachovska@gmail.com>
2021-05-29 16:26:50 +0200

    oprava class na ClassName u stránky index.js

--- pages/index.js
+++ pages/index.js
@@ -8,33 +8,33 @@ export default function Home({ allOrganizationsData: allOrganizationsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <div class="index-container">
-        <div class="index-intro">
-          <h1 class="headings index-heading">Pomůžeme Vám pečovat doma</h1>
-          <p class="intro__text">
+      <div className="index-container">
+        <div className="index-intro">
+          <h1 className="headings index-heading">Pomůžeme Vám pečovat doma</h1>
+          <p className="intro__text">
             Náš web Vás rychle přivede ke kontaktu na organizace, které nabízejí
             pomocnou ruku. Díky nám se zorientujete i v přehledu služeb těchto
             organizací a finančního úhrady služeb.
           </p>
         </div>
-        <div class="index-img">
-          <div class="index-box index-box-1">
+        <div className="index-img">
+          <div className="index-box index-box-1">
             <p>
               Jakou službu potřebujete? Zjistíte to lehce pomocí našeho
               speciálně sestaveného dotazníku.
             </p>
             <Link href={`/form`}>
-              <a class="btn btn-index bold">O koho pečujete?</a>
+              <a className="btn btn-index bold">O koho pečujete?</a>
             </Link>
           </div>
-          <div class="index-box index-box-2">
+          <div className="index-box index-box-2">
             <p>
               Jaké organizace působí v&nbsp;místě, kde pečujete o&nbsp;svého
               blízkého? Vyhledávat můžete podle nejbližší obce s&nbsp;přenesenou
               působností.
             </p>
             <Link href={`organizations`}>
-              <a class="btn btn-index bold">Kde pečujete?</a>
+              <a className="btn btn-index bold">Kde pečujete?</a>
             </Link>
           </div>
         </div>
