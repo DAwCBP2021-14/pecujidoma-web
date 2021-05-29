7f21096cc2273cb4a43e4b46d3461e1073a4dee6
Barbora Drachovská <bdrachovska@gmail.com>
2021-05-29 16:24:53 +0200

    oprava class na Classname u stránky abou.js

--- pages/about.js
+++ pages/about.js
@@ -7,8 +7,8 @@ export default function Page({ allOrganizationsData: allOrganizationsData }) {
       <Head>
         <title>O nás</title>
       </Head>
-      <div class="aboutUs-container">
-        <h3 class="headings">O nás</h3>
+      <div className="aboutUs-container">
+        <h3 className="headings">O nás</h3>
         <p>
           Vlastní zkušenost s péčí o blízké nás přivedla k vytvoření tohoto
           webu. Přináší rychlé a přehledné informace o tom, kdo a jak Vám může
@@ -16,9 +16,9 @@ export default function Page({ allOrganizationsData: allOrganizationsData }) {
           projektu se mohou zapojit organizace, které poskytují služby pro
           pečující.
         </p>
-        <div class="aboutUs-heros">
+        <div className="aboutUs-heros">
           <p>
-            <span class="bold">Autorky webu: </span> Barbora Drachovská,
+            <span className="bold">Autorky webu: </span> Barbora Drachovská,
             Kristýna Říhová, Ivana Kubičková  
           </p>
         </div>
