840d34d66bc550b97ce7f9553b65c1a4d3c103fb
Barbora Drachovská <bdrachovska@gmail.com>
2021-05-29 15:00:26 +0200

    úprava stránky přehled služeb

--- pages/services.js
+++ pages/services.js
@@ -1,16 +1,22 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
 
 export default function Page({ allOrganizationsData: allOrganizationsData }) {
-    return (
-        <Layout page>
-            <Head>
-                <title>Přehled služeb</title>
-            </Head>
-            <section>
-                <h1>Přehled služeb</h1>
-                <p>@todo</p>
-            </section>
-        </Layout>
-    )
+  return (
+    <Layout page>
+      <Head>
+        <title>Přehled služeb</title>
+      </Head>
+      <div class="index-container listOfServices__background--img">
+        <div class="index-intro">
+          <h2 class="headings index-heading">Přehled služeb</h2>
+          <p class="intro__text listOfServices__intro--background">
+            Veškeré služby, které vám mohou pomoci v péči o vaše blízké, najdete
+            přehledně na této stránce. Popisy jednotlivých služeb vám pomohou s
+            výběrem té správné pomocné ruky právě pro vás a vaše blízké.
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
 }
