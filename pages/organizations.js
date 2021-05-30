370ecae4bf0b88d1cfb7e7581e08fe3566df5ad1
KristynRiho <rihokristyn@gmail.com>
2021-05-30 14:01:20 +0200

    částečné nastylování vypisu hlášky po vyfiltrování

--- pages/organizations.js
+++ pages/organizations.js
@@ -5,7 +5,10 @@ import Image from 'next/image';
 import { getSortedOrganizationsData } from '../lib/organizations';
 import { useRouter } from 'next/router';
 
-export default function Page({ allOrganizationsData: allOrganizationsData, allOrganizationsPagesCount: allOrganizationsPagesCount }) {
+export default function Page({
+  allOrganizationsData: allOrganizationsData,
+  allOrganizationsPagesCount: allOrganizationsPagesCount,
+}) {
   const router = useRouter();
 
   const sortChanged = async (event) => {
@@ -17,7 +20,7 @@ export default function Page({ allOrganizationsData: allOrganizationsData, allOr
         page: currentPage,
         sort: event.target.value,
       },
-    })
+    });
   };
 
   return (
@@ -39,20 +42,21 @@ export default function Page({ allOrganizationsData: allOrganizationsData, allOr
           <div className="org-sorted">
             {router.query.town ? (
               <>
-                <span>Máte aktivní filtrování z přehledu služeb.</span>
-                {' '}
-                <Link href={`/organizations/`}>
-                  <a className="btn card-org__town">
-                    <span className="bold">Zrušit</span>
-                  </a>
-                </Link>
+                <div className="message">
+                  <p>Máte aktivní filtrování z přehledu služeb</p>{' '}
+                  <Link href={`/organizations/`}>
+                    <a className="btn btn-message">
+                      <span className="bold">Zrušit</span>
+                    </a>
+                  </Link>
+                </div>
               </>
             ) : (
               <label htmlFor="org">
                 <select
                   id="org"
                   name="sort"
-                  value={(router.query.sort ?? "name")}
+                  value={router.query.sort ?? 'name'}
                   onChange={sortChanged}
                 >
                   <option disabled>Vyberte způsob řazení</option>
@@ -63,7 +67,49 @@ export default function Page({ allOrganizationsData: allOrganizationsData, allOr
             )}
           </div>
 
-          {allOrganizationsData.map(({ id, name, town, url, logo }) => (
+          {allOrganizationsData && allOrganizationsData.length > 0 ? (
+            <>
+              {allOrganizationsData.map(({ id, name, town, url, logo }) => (
+                <div className="card-org" key={id}>
+                  <div className="card-org-flex">
+                    <div className="card-org__item card-org__item--logo">
+                      <div className="card-org__logo">
+                        <Image
+                          src={`/img/loga/${logo}`}
+                          alt={name}
+                          width={400}
+                          height={300}
+                          objectFit="contain"
+                        />
+                      </div>
+                    </div>
+                    <div className="card-org__item card-org__item--name">
+                      <h2 className="card-org__name">{name}</h2>
+                      <p className="card-org__town">{town}</p>
+                      <p className="card-org__www">
+                        <a href={url} target="_blank">
+                          {url}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                  <Link href={`/organizations/${id}`}>
+                    <a className="btn card-org__town">
+                      <span className="bold">Detail</span>
+                    </a>
+                  </Link>
+                </div>
+              ))}
+            </>
+          ) : (
+            <>
+              <p className="message message-nic">
+                Vaše mu fitlrování neodpovídají žádné organizace
+              </p>
+            </>
+          )}
+
+          {/* {allOrganizationsData.map(({ id, name, town, url, logo }) => (
             <div className="card-org" key={id}>
               <div className="card-org-flex">
                 <div className="card-org__item card-org__item--logo">
@@ -93,21 +139,32 @@ export default function Page({ allOrganizationsData: allOrganizationsData, allOr
                 </a>
               </Link>
             </div>
-          ))}
-
+          ))} */}
           {router.query.town ? (
-            <>
-            </>
+            <></>
           ) : (
             <nav className="pages" aria-label="Page navigation example">
               <ul className="pagination justify-content-center">
-                {Array.from(Array(allOrganizationsPagesCount).keys()).map(pageIndex => (
-                  <li className={(router.query.page ?? 1) == (pageIndex + 1) ? 'page-item active' : 'page-item'} key={pageIndex}>
-                    <Link href={`/organizations?page=${pageIndex + 1}&sort=${router.query.sort}`}>
-                      <a className="page-link">{pageIndex + 1}</a>
-                    </Link>
-                  </li>
-                ))}
+                {Array.from(Array(allOrganizationsPagesCount).keys()).map(
+                  (pageIndex) => (
+                    <li
+                      className={
+                        (router.query.page ?? 1) == pageIndex + 1
+                          ? 'page-item active'
+                          : 'page-item'
+                      }
+                      key={pageIndex}
+                    >
+                      <Link
+                        href={`/organizations?page=${pageIndex + 1}&sort=${
+                          router.query.sort
+                        }`}
+                      >
+                        <a className="page-link">{pageIndex + 1}</a>
+                      </Link>
+                    </li>
+                  ),
+                )}
               </ul>
             </nav>
           )}
@@ -118,11 +175,12 @@ export default function Page({ allOrganizationsData: allOrganizationsData, allOr
 }
 
 export async function getServerSideProps(context) {
-  let sortBy = context.query.sort
-  let page = context.query.page
-  let town = context.query.town
-  let services = context.query.services
-  const [allOrganizationsData, allOrganizationsPagesCount] = getSortedOrganizationsData(sortBy, page, town, services);
+  let sortBy = context.query.sort;
+  let page = context.query.page;
+  let town = context.query.town;
+  let services = context.query.services;
+  const [allOrganizationsData, allOrganizationsPagesCount] =
+    getSortedOrganizationsData(sortBy, page, town, services);
   return {
     props: {
       allOrganizationsData: allOrganizationsData,
