efdb1f79dda2146472f7cb3e4976986df8f51ed9
Barbora Drachovská <bdrachovska@gmail.com>
2021-06-06 10:43:09 +0200

    oprava prvního buttonu v sekci přehled služeb

--- pages/services.js
+++ pages/services.js
@@ -61,222 +61,224 @@ export default function Page({ allOrganizationsData: allOrganizationsData }) {
                   bude poskytnuta profesionální péče a vy získáte trochu volného
                   času.
                 </div>
-                <div className="services__accordion--button">
-                  <Link href={`/organizations?services=service1`}>
-                    <a className="btn btn-list_of_services">
-                      <span className="bold">
-                        Organizace poskytující terénní odlehčovací služby
-                      </span>
-                    </a>
-                  </Link>
+              </div>
+              <div className="services__accordion--button">
+                <Link href={`/organizations?services=service1`}>
+                  <a className="btn btn-list_of_services">
+                    <span className="bold">
+                      Organizace poskytující terénní odlehčovací služby
+                    </span>
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="flush-headingTwo">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseTwo"
+                aria-expanded="false"
+                aria-controls="flush-collapseTwo"
+              >
+                Paliativní péče
+                <span className="pruhledne">11111111111111111</span>
+                <sub>
+                  Péče o nevyléčitelně nemocné pacienty v domácích podmínkách.
+                </sub>
+              </button>
+            </h2>
+            <div
+              id="flush-collapseTwo"
+              className="accordion-collapse collapse text_flex"
+              aria-labelledby="flush-headingTwo"
+              data-bs-parent="#flush-headingTwo"
+            >
+              <div className="services__accordion--display">
+                <Image
+                  src={`/img/Ikony/Ikona_hospic.png`}
+                  alt={`paliativní péče`}
+                  width={150}
+                  height={150}
+                  objectFit="contain"
+                  className="listOfServices_img"
+                />
+                <div className="accordion-body">
+                  Péče o nevyléčitelně nemocné pacienty v domácích podmínkách.
+                  Domácí hospicová péče nabízí pomoc zdravotních sester i
+                  pečovatelek 24 hodin denně 7 dní v týdnu. Pečující mohou
+                  využít pomoc při dávkování léků, zdravotní péči, s hygienou
+                  nemocného nebo psychologickou a sociální podporu.
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingTwo">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseTwo"
-              aria-expanded="false"
-              aria-controls="flush-collapseTwo"
-            >
-              Paliativní péče
-              <span className="pruhledne">11111111111111111</span>
-              <sub>
-                Péče o nevyléčitelně nemocné pacienty v domácích podmínkách.
-              </sub>
-            </button>
-          </h2>
-          <div
-            id="flush-collapseTwo"
-            className="accordion-collapse collapse text_flex"
-            aria-labelledby="flush-headingTwo"
-            data-bs-parent="#flush-headingTwo"
-          >
-            <div className="services__accordion--display">
-              <Image
-                src={`/img/Ikony/Ikona_hospic.png`}
-                alt={`paliativní péče`}
-                width={150}
-                height={150}
-                objectFit="contain"
-                className="listOfServices_img"
-              />
-              <div className="accordion-body">
-                Péče o nevyléčitelně nemocné pacienty v domácích podmínkách.
-                Domácí hospicová péče nabízí pomoc zdravotních sester i
-                pečovatelek 24 hodin denně 7 dní v týdnu. Pečující mohou využít
-                pomoc při dávkování léků, zdravotní péči, s hygienou nemocného
-                nebo psychologickou a sociální podporu.
+              <div className="services__accordion--button">
+                <Link href={`/organizations?services=service2`}>
+                  <a className="btn btn-list_of_services">
+                    <span className="bold">
+                      Organizace poskytující paliativní péči
+                    </span>
+                  </a>
+                </Link>
               </div>
             </div>
-            <div className="services__accordion--button">
-              <Link href={`/organizations?services=service2`}>
-                <a className="btn btn-list_of_services">
-                  <span className="bold">
-                    Organizace poskytující paliativní péči
-                  </span>
-                </a>
-              </Link>
-            </div>
           </div>
-        </div>
 
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingThree">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseThree"
-              aria-expanded="false"
-              aria-controls="flush-collapseThree"
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="flush-headingThree">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapseThree"
+                aria-expanded="false"
+                aria-controls="flush-collapseThree"
+              >
+                Osobní asistence
+                <span className="pruhledne">111111111111111</span>
+                <sub>
+                  Pomoc a podpora pro pacienty při chodu domácnosti, hygieně
+                  nebo při sociálních kontaktech.
+                </sub>
+              </button>
+            </h2>
+            <div
+              id="flush-collapseThree"
+              className="accordion-collapse collapse text_flex"
+              aria-labelledby="flush-headingThree"
+              data-bs-parent="#flush-headingThree"
             >
-              Osobní asistence<span className="pruhledne">111111111111111</span>
-              <sub>
-                Pomoc a podpora pro pacienty při chodu domácnosti, hygieně nebo
-                při sociálních kontaktech.
-              </sub>
-            </button>
-          </h2>
-          <div
-            id="flush-collapseThree"
-            className="accordion-collapse collapse text_flex"
-            aria-labelledby="flush-headingThree"
-            data-bs-parent="#flush-headingThree"
-          >
-            <div className="services__accordion--display">
-              <Image
-                src={`/img/Ikony/Ikona_osobni_asistence.png`}
-                alt={`osobní asistence`}
-                width={150}
-                height={150}
-                objectFit="contain"
-                className="listOfServices_img"
-              />
-              <div className="accordion-body">
-                Pomoc a podpora pro pacienty při chodu domácnosti, hygieně, se
-                sociálními kontakty a dalšími potřebami. Osobní asistence je
-                placená sociální služba pomáhající osobám, které z důvodu
-                zdravotního postižení, věku nebo nemoci potřebují pomoc v
-                běžných činnostech života.
+              <div className="services__accordion--display">
+                <Image
+                  src={`/img/Ikony/Ikona_osobni_asistence.png`}
+                  alt={`osobní asistence`}
+                  width={150}
+                  height={150}
+                  objectFit="contain"
+                  className="listOfServices_img"
+                />
+                <div className="accordion-body">
+                  Pomoc a podpora pro pacienty při chodu domácnosti, hygieně, se
+                  sociálními kontakty a dalšími potřebami. Osobní asistence je
+                  placená sociální služba pomáhající osobám, které z důvodu
+                  zdravotního postižení, věku nebo nemoci potřebují pomoc v
+                  běžných činnostech života.
+                </div>
+              </div>
+              <div className="services__accordion--button">
+                <Link href={`/organizations?services=service3`}>
+                  <a className="btn btn-list_of_services">
+                    <span className="bold">
+                      Organizace poskytující osobní asistenci
+                    </span>
+                  </a>
+                </Link>
               </div>
             </div>
-            <div className="services__accordion--button">
-              <Link href={`/organizations?services=service3`}>
-                <a className="btn btn-list_of_services">
-                  <span className="bold">
-                    Organizace poskytující osobní asistenci
-                  </span>
-                </a>
-              </Link>
-            </div>
           </div>
-        </div>
 
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-heading3">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapse3"
-              aria-expanded="false"
-              aria-controls="flush-collapse3"
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="flush-heading3">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapse3"
+                aria-expanded="false"
+                aria-controls="flush-collapse3"
+              >
+                Psychologická péče
+                <span className="pruhledne">1111111111111</span>
+                <sub>
+                  Psychologická pomoc pro pacienty i jejich rodiny, zejména o
+                  pečující členy domácnosti je velmi potřebná.
+                </sub>
+              </button>
+            </h2>
+            <div
+              id="flush-collapse3"
+              className="accordion-collapse collapse text_flex"
+              aria-labelledby="flush-heading3"
+              data-bs-parent="#accordionFlushExample"
             >
-              Psychologická péče<span className="pruhledne">1111111111111</span>
-              <sub>
-                Psychologická pomoc pro pacienty i jejich rodiny, zejména o
-                pečující členy domácnosti je velmi potřebná.
-              </sub>
-            </button>
-          </h2>
-          <div
-            id="flush-collapse3"
-            className="accordion-collapse collapse text_flex"
-            aria-labelledby="flush-heading3"
-            data-bs-parent="#accordionFlushExample"
-          >
-            <div className="services__accordion--display">
-              <Image
-                src={`/img/Ikony/Ikona_psycholog.png`}
-                alt={`psychologická péče`}
-                width={150}
-                height={150}
-                objectFit="contain"
-                className="listOfServices_img"
-              />
-              <div className="accordion-body">
-                Psychologická pomoc pro pacienty i jejich rodiny, zejména o
-                pečující členy domácnosti, je velmi potřebná. Psycholog
-                vyslechne, poradí a pomůže v těžké situaci. Nebojte se obrátit
-                na odborníky, kteří vám mohou v náročném životním období
-                ulehčit.
+              <div className="services__accordion--display">
+                <Image
+                  src={`/img/Ikony/Ikona_psycholog.png`}
+                  alt={`psychologická péče`}
+                  width={150}
+                  height={150}
+                  objectFit="contain"
+                  className="listOfServices_img"
+                />
+                <div className="accordion-body">
+                  Psychologická pomoc pro pacienty i jejich rodiny, zejména o
+                  pečující členy domácnosti, je velmi potřebná. Psycholog
+                  vyslechne, poradí a pomůže v těžké situaci. Nebojte se obrátit
+                  na odborníky, kteří vám mohou v náročném životním období
+                  ulehčit.
+                </div>
+              </div>
+              <div className="services__accordion--button">
+                <Link href={`/organizations?services=service4`}>
+                  <a className="btn btn-list_of_services">
+                    <span className="bold">
+                      Organizace poskytující psychologickou péči
+                    </span>
+                  </a>
+                </Link>
               </div>
             </div>
-            <div className="services__accordion--button">
-              <Link href={`/organizations?services=service4`}>
-                <a className="btn btn-list_of_services">
-                  <span className="bold">
-                    Organizace poskytující psychologickou péči
-                  </span>
-                </a>
-              </Link>
-            </div>
           </div>
-        </div>
 
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-heading4">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapse4"
-              aria-expanded="false"
-              aria-controls="flush-collapse4"
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="flush-heading4">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#flush-collapse4"
+                aria-expanded="false"
+                aria-controls="flush-collapse4"
+              >
+                Půjčovna kompenzačních pomůcek
+                <sub>
+                  Velká pomoc pro pečující rodiny i jejich pacienty jsou různé
+                  kompenzační pomůcky.
+                </sub>
+              </button>
+            </h2>
+            <div
+              id="flush-collapse4"
+              className="accordion-collapse collapse text_flex"
+              aria-labelledby="flush-heading4"
+              data-bs-parent="#flush-heading4"
             >
-              Půjčovna kompenzačních pomůcek
-              <sub>
-                Velká pomoc pro pečující rodiny i jejich pacienty jsou různé
-                kompenzační pomůcky.
-              </sub>
-            </button>
-          </h2>
-          <div
-            id="flush-collapse4"
-            className="accordion-collapse collapse text_flex"
-            aria-labelledby="flush-heading4"
-            data-bs-parent="#flush-heading4"
-          >
-            <div className="services__accordion--display">
-              <Image
-                src={`/img/Ikony/Ikona_pujcovna.png`}
-                alt={`půjčovna kompenzačních pomůcek`}
-                width={150}
-                height={150}
-                objectFit="contain"
-                className="listOfServices_img"
-              />
-              <div className="accordion-body">
-                Velká pomoc pro pečující rodiny i jejich pacienty jsou různé
-                kompenzační pomůcky, které pomohou ulehčit péči i každodenní
-                běžné fungování rodiny. Půjčovny kompenzačních pomůcek nabízejí
-                mnoho možností od polohovacích postelí, vozíků, chodítek, až po
-                oxygenátory či lienární dávkovače léků.
+              <div className="services__accordion--display">
+                <Image
+                  src={`/img/Ikony/Ikona_pujcovna.png`}
+                  alt={`půjčovna kompenzačních pomůcek`}
+                  width={150}
+                  height={150}
+                  objectFit="contain"
+                  className="listOfServices_img"
+                />
+                <div className="accordion-body">
+                  Velká pomoc pro pečující rodiny i jejich pacienty jsou různé
+                  kompenzační pomůcky, které pomohou ulehčit péči i každodenní
+                  běžné fungování rodiny. Půjčovny kompenzačních pomůcek
+                  nabízejí mnoho možností od polohovacích postelí, vozíků,
+                  chodítek, až po oxygenátory či lienární dávkovače léků.
+                </div>
               </div>
-            </div>
-            <div className="services__accordion--button">
-              <Link href={`/organizations?services=service5`}>
-                <a className="btn btn-list_of_services">
-                  <span className="bold">Půjčovny kompenzačních pomůcek</span>
-                </a>
-              </Link>
+              <div className="services__accordion--button">
+                <Link href={`/organizations?services=service5`}>
+                  <a className="btn btn-list_of_services">
+                    <span className="bold">Půjčovny kompenzačních pomůcek</span>
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
